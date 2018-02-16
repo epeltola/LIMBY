9eb4eab0a2930e15188c9557738d4fe09af7ea45
Kevin Hsieh <kevin899@gmail.com>
2018-02-15 23:00:38 -0800

    Fix graph appearance

--- LIMBY/LIMBY/LineChartViewController.swift
+++ LIMBY/LIMBY/LineChartViewController.swift
@@ -11,40 +11,42 @@ import UIKit
 import Charts
 
 class LineChartViewController: UIViewController, UITextFieldDelegate {
- 
+
     @IBOutlet var lineChartView: LineChartView!
     @IBOutlet weak var segmentedController: UISegmentedControl!
     
-    // Initially load delegate
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // If there is no data
         lineChartView.noDataText = "No data available."
     }
     
-    // Load the graph before view appears. We do this here because data may change
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
         let sampleData = [1.0, 5.0, 3.0, 2.0, 4.0]
-        setLineChart(timeRange: segmentedController.selectedSegmentIndex, values: sampleData)
+        setLineChart(timeRange: segmentedController.selectedSegmentIndex,
+                     values: sampleData)
     }
     
-    // Set Line Graph
     func setLineChart(timeRange: Int, values: [Double]) {
-        // let barChartFormatter:BarChartFormatterWeek = BarChartFormatterWeek()
-        // let xAxis:XAxis = XAxis()
-        
+
+        // Process data
         var dataEntries: [ChartDataEntry] = []
-        
         for i in 0..<values.count {
             dataEntries.append(ChartDataEntry(x: Double(i), y: values[i]))
-            
             // let _ = barChartFormatter.stringForValue(Double(i), axis: xAxis)
         }
+        let dataSet = LineChartDataSet(values: dataEntries, label: "Bird 1")
+        dataSet.axisDependency = .left
         
-        let dataSet = LineChartDataSet(values: dataEntries, label: "Generic Perch Data")
+        // Add data
+        lineChartView.data = LineChartData(dataSet: dataSet)
+        lineChartView.data?.setValueFont(UIFont.systemFont(ofSize: 11))
+        
+        // Other code
+        // ---------------------------------------------------------------------
+        
+        // let barChartFormatter:BarChartFormatterWeek = BarChartFormatterWeek()
+        // let xAxis:XAxis = XAxis()
         
         // xAxis.valueFormatter = barChartFormatter
         // barGraphView.xAxis.valueFormatter = xAxis.valueFormatter
@@ -53,7 +55,7 @@ class LineChartViewController: UIViewController, UITextFieldDelegate {
         // let average = BudgetVariables.calculateAverage(nums: values)
         
         // Remove the average line from the previous graph
-        lineChartView.rightAxis.removeAllLimitLines();
+        //lineChartView.rightAxis.removeAllLimitLines();
         
         // Only add the average line if there is actually data in the bar graph
         /*if average != 0.0
@@ -66,23 +68,11 @@ class LineChartViewController: UIViewController, UITextFieldDelegate {
             barGraphView.rightAxis.addLimitLine(ll)
         }*/
         
-        // Set the position of the x axis label
-        // lineChartView.rightAxis.axisMinimum = 0
-        // lineChartView.xAxis.labelPosition = .bottom
-        
         // Select the color scheme
         // chartDataSet.colors = ColorArray[BudgetVariables.budgetArray[BudgetVariables.currentIndex].barGraphColor]
         
-        // chartDataSet.axisDependency = .right
-        lineChartView.data = LineChartData(dataSet: dataSet)
-        
-        // Legend font size
-        // barGraphView.legend.font = UIFont.systemFont(ofSize: 13)
-        // barGraphView.legend.formSize = 8
-        
         // Defaults
-        // chartData.setDrawValues(true)
-        // barGraphView.rightAxis.drawLabelsEnabled = true
+        //lineChartView.data?.setDrawValues(false)
         
         /*if BudgetVariables.budgetArray[BudgetVariables.currentIndex].historyArray.isEmpty == true || BudgetVariables.isAllZeros(array: values) == true {
             chartData.setDrawValues(false)
@@ -90,43 +80,51 @@ class LineChartViewController: UIViewController, UITextFieldDelegate {
         }*/
         
         // Set where axis starts
-        // barGraphView.setScaleMinima(0, scaleY: 0.0)
-        
-        // Customization
-        /*barGraphView.pinchZoomEnabled = false
-        barGraphView.scaleXEnabled = false
-        barGraphView.scaleYEnabled = false
-        barGraphView.xAxis.drawGridLinesEnabled = false
-        barGraphView.leftAxis.drawGridLinesEnabled = false
-        barGraphView.rightAxis.drawGridLinesEnabled = false
-        barGraphView.leftAxis.drawLabelsEnabled = false
-        barGraphView.rightAxis.spaceBottom = 0
-        barGraphView.leftAxis.spaceBottom = 0*/
-        
-        // Set font size
-        // chartData.setValueFont(UIFont.systemFont(ofSize: 12))
+        //lineChartView.setScaleMinima(0, scaleY: 0.0)
         
         /* let format = NumberFormatter()
         format.numberStyle = .currency
         let formatter = DefaultValueFormatter(formatter: format)
         chartData.setValueFormatter(formatter) */
         
-        // Set Y Axis Font
-        // barGraphView.rightAxis.labelFont = UIFont.systemFont(ofSize: 11)
-        
-        // Set X Axis Font
-        // barGraphView.xAxis.labelFont = UIFont.systemFont(ofSize: 13)
-        
         // Force all 7 x axis labels to show up
         // barGraphView.xAxis.setLabelCount(7, force: false)
+        // ---------------------------------------------------------------------
+
         
-        // Set description texts
+        // x-axis
+        //lineChartView.xAxis.axisMinimum = 0
+        //lineChartView.xAxis.drawLabelsEnabled = true
+        lineChartView.xAxis.labelFont = UIFont.systemFont(ofSize: 11)
+        //lineChartView.xAxis.drawGridLinesEnabled = false
+        lineChartView.xAxis.labelPosition = .bottom
+        
+        // Left y-axis
+        lineChartView.leftAxis.axisMinimum = 0
+        //lineChartView.leftAxis.drawLabelsEnabled = false
+        lineChartView.leftAxis.labelFont = UIFont.systemFont(ofSize: 11)
+        //lineChartView.leftAxis.drawGridLinesEnabled = false
+        //lineChartView.leftAxis.spaceBottom = 0
+ 
+        // Right y-axis
+        //lineChartView.rightAxis.axisMinimum = 0
+        lineChartView.rightAxis.drawLabelsEnabled = false
+        //lineChartView.rightAxis.labelFont = UIFont.systemFont(ofSize: 11)
+        lineChartView.rightAxis.drawGridLinesEnabled = false
+        //lineChartView.rightAxis.spaceBottom = 0
+        
+        // Description & legend
         lineChartView.chartDescription?.text = "This is the description."
+        lineChartView.legend.font = UIFont.systemFont(ofSize: 11)
+        //lineChartView.legend.formSize = 8
         
-        // Set the background color
-        // barGraphView.backgroundColor = UIColor(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)
+        // Interaction
+        lineChartView.backgroundColor = UIColor.white
+        lineChartView.pinchZoomEnabled = false
+        lineChartView.scaleXEnabled = false
+        lineChartView.scaleYEnabled = false
         
-        // Animate the chart
-        lineChartView.animate(xAxisDuration: 0.0, yAxisDuration: 1.5)
+        // Animate
+        lineChartView.animate(xAxisDuration: 0.0, yAxisDuration: 1.0)
     }
 }
