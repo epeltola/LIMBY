57a90b79fa57a785d69d1d578476004b171aafc9
Kevin Hsieh <kevin899@gmail.com>
2018-02-18 00:39:36 -0800

    Set y-axis granularity, code cleanup

--- LIMBY/LIMBY/LineChartViewController.swift
+++ LIMBY/LIMBY/LineChartViewController.swift
@@ -12,15 +12,15 @@ import Charts
 
 class LineChartViewController: UIViewController, UITextFieldDelegate {
     
-    // --------------
+    // -------------------------------------------------------------------------
     // Initialization
-    // --------------
+    // -------------------------------------------------------------------------
 
     // Static class constants
-    static let LEGEND_SQUARE_SIZE = CGFloat(16)
     static let CHART_FONT = UIFont.systemFont(ofSize: 11)
-    static let YLABEL_COUNT = 9
+    static let LEGEND_SQUARE_SIZE = CGFloat(16)
     static let NATHANS_CONSTANT = CGFloat(17)
+    static let YLABEL_COUNT = 10
     
     // View did load actions
     override func viewDidLoad() {
@@ -31,14 +31,12 @@ class LineChartViewController: UIViewController, UITextFieldDelegate {
     // View will appear actions
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        let sampleData = [1.0, 5.0, 3.0, 2.0, 4.0, 1.0, 5.0, 3.0, 2.0]
-        plotLineChart(timeRange: segmentedController.selectedSegmentIndex,
-                      values: sampleData)
+        plotLineChart(timeRange: segmentedController.selectedSegmentIndex)
     }
     
-    // ------------------
+    // -------------------------------------------------------------------------
     // IBOutlet variables
-    // ------------------
+    // -------------------------------------------------------------------------
     
     // Line chart instance
     @IBOutlet var lineChartView: LineChartView!
@@ -46,110 +44,89 @@ class LineChartViewController: UIViewController, UITextFieldDelegate {
     // Segment selector instance
     @IBOutlet weak var segmentedController: UISegmentedControl!
     
-    // -----------------
+    // -------------------------------------------------------------------------
     // IBAction handlers
-    // -----------------
+    // -------------------------------------------------------------------------
     
     // Modify line chart whenever segment index changes
     @IBAction func segmentChanged(_ sender: Any) {
-        let index = segmentedController.selectedSegmentIndex
-        switch index {
-        case 0:
-            let sampleData = [1.0, 5.0, 3.0, 2.0,
-                              4.0, 1.0, 5.0, 3.0,
-                              2.0]
-            plotLineChart(timeRange: index, values: sampleData)
-        case 1:
-            let sampleData = [5.0, 3.0, 2.0, 4.0,
-                              1.0, 2.0, 8.0]
-            plotLineChart(timeRange: index, values: sampleData)
-        case 2:
-            let sampleData = [3.0, 1.0, 4.0, 1.0,
-                              5.0, 4.0, 1.0, 2.0,
-                              4.0, 9.0, 2.0, 8.0,
-                              12.0, 4.0, 5.0, 2.0,
-                              3.0, 1.0, 4.0, 1.0,
-                              5.0, 4.0, 1.0, 2.0,
-                              4.0, 9.0, 2.0, 8.0,
-                              3.0, 1.0, 4.0]
-            plotLineChart(timeRange: index, values: sampleData)
-        case 3:
-            let sampleData = [2.0, 4.0, 1.0, 2.0,
-                              7.0, 4.0, 1.0, 5.0,
-                              10.0, 2.0, 8.0, 3.0]
-            plotLineChart(timeRange: index, values: sampleData)
-        default:
-            print("Invalid segment index")
-        }
+        plotLineChart(timeRange: segmentedController.selectedSegmentIndex)
     }
     
-    // ----------------
+    // -------------------------------------------------------------------------
     // Helper functions
-    // ----------------
+    // -------------------------------------------------------------------------
     
-    // Get past 7 or 31 days into String array in [MM/DD] format
-    func getDateInterval(interval: String) -> [String] {
-        var size = 7
-        if (interval == "Month") {
-            size = 31
-        }
+    // Get the specified number of past dates in [MM/DD] format
+    func getPastDates(days: Int) -> [String] {
         let cal = Calendar.current
-        var dayIndex = cal.date(byAdding: .day, value: ((size - 1) * -1), to: cal.startOfDay(for: Date()))
-        var days = [String]()
-        for _ in 1 ... size {
-            let day = cal.component(.day, from: dayIndex!)
-            let month = cal.component(.month, from: dayIndex!)
-            days.append(String(month) + "/" + String(day))
-            dayIndex = cal.date(byAdding: .day, value: 1, to: dayIndex!)!
-        }
-        return days
+        let startDate = cal.date(byAdding: .day, value: -days + 1,
+                                 to: cal.startOfDay(for: Date()))!
+        return (0..<days).map({ i -> String in
+            let date = cal.date(byAdding: .day, value: i, to: startDate)!
+            return String(cal.component(.month, from: date)) + "/" +
+                   String(cal.component(.day, from: date))
+        })
     }
     
-    // Get past twelve months into String array in [Mmm] format
-    func getPastTwelveMonths() -> [String] {
-        let cal = Calendar.current
-        let monthIndex = cal.component(.month, from: cal.startOfDay(for: Date()))
-        var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
-                      "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-        let monthSliceArray = months[monthIndex...monthIndex + 11]
-        return [String](Array(monthSliceArray))
+    // Get x-axis labels based on the selected timeRange
+    func getXLabels(timeRange: Int) -> [String] {
+        switch timeRange {
+        case 0:  // day
+            return ["12 AM", "3 AM", "6 AM", "9 AM",
+                    "12 PM", "3 PM", "6 PM", "9 PM", "12 AM"]
+        case 1:  // week
+            return getPastDates(days: 7)
+        case 2:  // month
+            return getPastDates(days: 31)
+        case 3:  // year
+            let cal = Calendar.current
+            let monthIndex =
+                cal.component(.month, from: cal.startOfDay(for: Date()))
+            let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                          "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+                          "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                          "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+            return Array(months[monthIndex...monthIndex + 11])
+        default:
+            print("getXLabels: Invalid timeRange!")
+            return []
+        }
+    }
+    
+    // Get sample values (for testing) based on the selected timeRange
+    func getSampleValues(timeRange: Int) -> [Double] {
+        switch timeRange {
+        case 0:  // day
+            return [1.0, 5.0, 3.0, 2.0, 4.0, 1.0, 5.0, 3.0, 2.0]
+        case 1:  // week
+            return [5.0, 3.0, 2.0, 4.0, 1.0, 2.0, 8.0]
+        case 2:  // month
+            return [3.0, 1.0, 4.0, 1.0, 5.0, 4.0, 1.0,
+                    2.0, 4.0, 9.0, 2.0, 8.0, 2.0, 4.0,
+                    5.0, 2.0, 3.0, 1.0, 4.0, 1.0, 5.0,
+                    4.0, 1.0, 2.0, 4.0, 9.0, 2.0, 8.0,
+                    3.0, 1.0, 4.0]
+        case 3:  // year
+            return [2.0, 4.0, 1.0, 2.0, 7.0, 4.0,
+                    1.0, 5.0, 10.0, 2.0, 8.0, 3.0]
+        default:
+            print("getSampleValues: Invalid timeRange!")
+            return []
+        }
     }
     
     // Plot line chart given a time interval and values
-    func plotLineChart(timeRange: Int, values: [Double]) {
-        
-        // Custom x-axis labels
-        var xLabels = [String]()
-        switch timeRange {
-        case 0:
-            xLabels = ["12 AM", "3 AM", "6 AM", "9 AM",
-                     "12 PM", "3 PM", "6 PM", "9 PM",
-                     "12 AM"]
-        case 1:
-            xLabels = getDateInterval(interval: "Week")
-        case 2:
-            xLabels = getDateInterval(interval: "Month")
-        case 3:
-            xLabels = getPastTwelveMonths()
-        default:
-            print("Invalid segment index")
-        }
+    func plotLineChart(timeRange: Int, inputValues: [Double]? = nil) {
+        let values = inputValues ?? getSampleValues(timeRange: timeRange)
+        let xLabels = getXLabels(timeRange: timeRange)
         lineChartView.xAxis.valueFormatter =
             IndexAxisValueFormatter(values: xLabels)
-        switch timeRange {
-        case 0, 1, 3:
-            lineChartView.xAxis.granularity = 1
-        case 2:
-            lineChartView.xAxis.granularity = 5
-        default:
-            lineChartView.xAxis.granularity = 1
-        }
         
         // Process data
-        var dataEntries: [ChartDataEntry] = []
-        for i in 0..<values.count {
-            dataEntries.append(ChartDataEntry(x: Double(i), y: values[i]))
-        }
+        let dataEntries = (0..<values.count).map({ i -> ChartDataEntry in
+            return ChartDataEntry(x: Double(i), y: values[i])
+        })
         let dataSet = LineChartDataSet(values: dataEntries,
                                        label: "Bird 1 weight (g)")
         dataSet.colors = [ChartColorTemplates.liberty()[3]]
@@ -165,8 +142,8 @@ class LineChartViewController: UIViewController, UITextFieldDelegate {
         lineChartView.leftAxis.removeAllLimitLines()
         let average = values.reduce(0, +) / Double(values.count)
         if average > 0.0 {
-            let ll = ChartLimitLine(limit: average,
-                                    label: "Average: " + String(format: "%.2f", average))
+            let ll = ChartLimitLine(limit: average, label: "Average: " +
+                                    String(format: "%.2f", average))
             ll.lineColor = UIColor.black
             ll.valueFont = LineChartViewController.CHART_FONT
             ll.lineWidth = 2
@@ -175,27 +152,38 @@ class LineChartViewController: UIViewController, UITextFieldDelegate {
         }
         
         // x-axis
-        switch timeRange {
-        case 0, 1, 3:
-            lineChartView.xAxis.labelCount = values.count
-        case 2:
-            lineChartView.xAxis.labelCount = 7
-        default:
-            lineChartView.xAxis.labelCount = values.count
-        }
-        lineChartView.xAxis.axisMinimum = 0
+        lineChartView.xAxis.axisMinimum = 0.0
         lineChartView.xAxis.labelFont = LineChartViewController.CHART_FONT
         lineChartView.xAxis.labelPosition = .bottom
-        lineChartView.xAxis.labelRotationAngle = -45
+        lineChartView.xAxis.labelRotationAngle = -45.0
+        switch timeRange {
+        case 0:  // day
+            lineChartView.xAxis.granularity = 1.0
+            lineChartView.xAxis.labelCount = 9
+        case 1:  // week
+            lineChartView.xAxis.granularity = 1.0
+            lineChartView.xAxis.labelCount = 7
+        case 2:  // month
+            lineChartView.xAxis.granularity = 5.0
+            lineChartView.xAxis.labelCount = 7
+        case 3:  // year
+            lineChartView.xAxis.granularity = 1.0
+            lineChartView.xAxis.labelCount = 12
+        default:
+            print("plotLineChart: Invalid timeRange!")
+        }
         
         // Left y-axis
-        lineChartView.leftAxis.axisMinimum = 0
+        lineChartView.leftAxis.axisMinimum = 0.0
         lineChartView.leftAxis.labelCount = LineChartViewController.YLABEL_COUNT
         lineChartView.leftAxis.labelFont = LineChartViewController.CHART_FONT
+        lineChartView.leftAxis.granularity = 1.0
+        if let max = values.max() {
+            lineChartView.leftAxis.axisMaximum = (max + 1).rounded(.towardZero)
+        }
  
         // Right y-axis
-        lineChartView.rightAxis.drawLabelsEnabled = false
-        lineChartView.rightAxis.drawGridLinesEnabled = false
+        lineChartView.rightAxis.enabled = false
         
         // Description & legend
         lineChartView.chartDescription?.text = ""
